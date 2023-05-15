64c68744cc433a64207377ad36190005153ef87d
tomi-kid <tomiketema209@gmail.com>
2023-05-15 05:31:34 -0700

    addoing storage constructure

--- models/__init__.py
+++ models/__init__.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""Instantiates an object of class FileStorage"""
-from models.engine.file_storage import FileStorage
-
-
-storage = FileStorage()
-storage.reload()#!/usr/bin/python3
-"""
+"""Instantiates an object of class FileStorage
 BaseModel contractur
 """
 from models.engine.file_storage import FileStorage
